2b9e235de66f0d9e3d05387580178d71c17055af
Liareth <liarethnwn@gmail.com>
2016-02-10 11:34:05 +0000

    Remove arelith-profiler, will be replaced soon with influxdb stuff.

--- plugins/arelith-profiler/hooks/AllHooks.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef NWNX_ALL_HOOKS_HPP_
-#define NWNX_ALL_HOOKS_HPP_
-
-#include "MainLoopHook.hpp"
-#include "RunScriptHook.hpp"
-#include "PrintLogEntryHook.hpp"
-#include "IntraTileDFSHook.hpp"
-
-#endif // NWNX_ALL_HOOKS_HPP_
